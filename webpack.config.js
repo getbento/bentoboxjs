cd92cfbf5a8d0d508cc9f398e55d34c210598984
Eric Fries <ericfries@gmail.com>
2016-03-15 17:38:11 -0400

    add underscore to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,18 @@ var webpack = require("webpack");
 
 module.exports = {
 	entry: __dirname + "/app/bentobox.js",
-	
+
 	output: {
 		path: __dirname + "/dist",
 		filename: "bentobox.dist.js"
-	}
-}
+	},
+
+    resolve:{
+        modulesDirectories: ['node_modules'],
+    },
+    plugins:[
+        new webpack.ProvidePlugin({
+            "_": "underscore"
+        })
+    ]
+};
