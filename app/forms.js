e125cd65166ea57beac43df3fa96a93ec8458be6
Eric Fries <ericfries@gmail.com>
2016-08-11 11:18:51 -0400

    remove input errors after successful form submission

--- app/forms.js
+++ app/forms.js
@@ -56,6 +56,7 @@ var Forms = (function() {
 			$(options.success).fadeIn();
 			options.successCallback();
 			forms.currentForm.trigger('reset');
+			forms.currentForm.find('input, select, textarea').removeClass('error');
 		} else {
 			forms.currentForm.find(options.error).fadeIn();
 			options.errorCallback();
